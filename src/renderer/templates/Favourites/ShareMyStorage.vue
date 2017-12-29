bad928d6c4e5ea049145c4da0abb2016303ad56f
lishi <lishi1608@gmail.com>
2017-12-29 13:37:17 +0800

    add img

--- src/renderer/templates/Favourites/ShareMyStorage.vue
+++ src/renderer/templates/Favourites/ShareMyStorage.vue
@@ -1,12 +1,23 @@
 <style scoped>
     div {
         padding: 14px;
+        text-align: center;
+    }
+    img {
+        margin: 20px auto;
+    }
+    h1 {
+        font-size: 1.6rem;
+        margin: 20px auto;
     }
 </style>
 <template>
     <div>
-        Genaro Share now on Beta. <br>
+        <img src="~@/assets/fly.png" alt="">
+        <h1>Genaro Share now on Beta</h1> 
+        <p>
         If you want to share your storage space please click this <a target="_blank" href="https://www.npmjs.com/package/genaroshare-daemon">link</a> for a detailed explanation how to install and run Genaro Share beta.
+        </p>
     </div>
 </template>
 
