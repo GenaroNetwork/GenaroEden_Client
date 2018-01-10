6c73117255a267e3bf4e076ef983b124845a5415
lishi <lishi1608@gmail.com>
2018-01-11 01:31:29 +0800

    use english as default locale

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -8,9 +8,11 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(ElementUI, { locale })
 Vue.prototype.$http = axios
 
-Vue.use(ElementUI)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
