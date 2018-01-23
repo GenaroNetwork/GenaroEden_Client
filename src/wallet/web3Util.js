9c124bafe62af8fc45a239fccdd23a69f7a322f3
lishi <lishi1608@gmail.com>
2018-01-23 19:31:26 +0800

    fix mainnet

--- src/wallet/web3Util.js
+++ src/wallet/web3Util.js
@@ -4,12 +4,12 @@ const isDev = process.env.NODE_ENV === 'development'
 let web3Provider, chainId, web3Instance, GNXAddr, EtherscanURL
 
 if(isDev) {
-    web3Provider = 'https://ropsten.infura.io/wYBhtj2SSUB7qlztqEjx'
+    web3Provider = 'https://ropsten.infura.io/CPKlwMsRTFVy6idI23Yb'
     chainId = 3
     GNXAddr = "0x1F84118c3B0f3f97c63B8e125456d76C78baBed5" //ropsten
     EtherscanURL = 'https://ropsten.etherscan.io/tx/'
 } else {
-    web3Provider = 'https://infura.io/wYBhtj2SSUB7qlztqEjx' // TODO: check
+    web3Provider = 'https://mainnet.infura.io/CPKlwMsRTFVy6idI23Yb'
     chainId = 0
     GNXAddr = "0x6cd6104d8bb94a08e0c5ce2486192894c34ee317"
     EtherscanURL = 'https://etherscan.io/tx/'
