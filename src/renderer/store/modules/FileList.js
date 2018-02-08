49967df03ceaaae6449c0d65727d7aaeea245145
孟子易 <mengziyi540841@gmail.com>
2018-02-08 21:21:26 +0800

    add await for async function

--- src/renderer/store/modules/FileList.js
+++ src/renderer/store/modules/FileList.js
@@ -21,20 +21,19 @@ let mutations = {
 };
 
 let actions = {
-    fileListLoadBucket({ commit, rootState, dispatch }, { bucketId }) {
+    async fileListLoadBucket({ commit, rootState, dispatch }, { bucketId }) {
         let bucket = rootState.BucketList.buckets.find(bucket => bucket.id === bucketId);
         commit("fileListLoadBucket", { bucket });
-        dispatch("fileListLoadFile");
+        await dispatch("fileListLoadFile");
     },
     async fileListLoadFile({ commit, state }) {
         if (!state.bucket) throw new Error("no current bucket displaying");
-
         let files = await new Bucket(state.bucket.id).list();
         commit("fileListLoadFile", { files });
     },
     async fileListDelete({ commit, state, dispatch }, { fileId }) {
         await new Bucket(state.bucket.id).delete({ fileId });
-        dispatch("fileListLoadFile");
+        await dispatch("fileListLoadFile");
     },
 };
 
