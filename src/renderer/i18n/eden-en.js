5ddadc308ebd7073ca698ecdda1b4325820286b0
lane tao <taoxiaolei87@163.com>
2018-02-24 14:35:43 +0800

    i18n eden-en & zh-CN file retranslated

--- src/renderer/i18n/eden-en.js
+++ src/renderer/i18n/eden-en.js
@@ -9,12 +9,12 @@ exports.default = {
       reset: 'Reset Password',
       usernameholder: 'User Name',
       passwordholder: 'Password',
-      inputname:'Please input username',
-      emailerr:'Incorrect email format',
-      inputpwd: 'Please input password',
-      pwdlength: 'Password length must not be less than 6 bits',
-      loginerr: 'Username Or Password Error',
-      logout: 'logout'
+      inputname:'Please Enter Username',
+      emailerr:'Incorrect Email Format',
+      inputpwd: 'Please Enter Password',
+      pwdlength: 'Password length must not be less than 6 characters',
+      loginerr: 'Username or Password Error',
+      logout: 'Log Out'
     },
     dialog: {
       submit: 'Submit',
@@ -32,7 +32,7 @@ exports.default = {
         delete: 'Delete',
         cancel: 'Cancel',
         title: 'Confirm Delete Folder: {name}',
-        message: 'All your files in folder {name} will be deleted. This action cannot be undone.'
+        message: 'All files in folder {name} will be deleted. This action cannot be undone.'
       },
       fileid: 'File ID',
       download: 'Download',
@@ -51,11 +51,11 @@ exports.default = {
       size: 'Size',
       created: 'Created',
       folder: 'Folder',
-      tip1: 'No upload/download history yet'
+      tip1: 'No Upload/Download History Yet'
     },
     mywallet: {
       mywallet: 'My Wallet',
-      account: 'Acount',
+      account: 'Account',
       deposit: 'Deposit',
       transfer: 'Transfer',
       hash: 'Hash',
@@ -64,40 +64,40 @@ exports.default = {
       from: 'From',
       to: 'To',
       amount: 'Amount',
-      recipientaddress:"Recippient Address", // the word 'Recippient' is written wrong?
+      recipientaddress:"Recipient Address", 
       ETH: 'ETH',
       GNX: 'GNX',
-      gasprice: 'gas price (Gwei)',
+      gasprice: 'Gas Price (Gwei)',
       gaspriceholder: 'Gas',
-      gaslimit: 'gas limit (Unit)',
+      gaslimit: 'Gas Limit (Unit)',
       gaslimitholder: 'Gas Limit',
       sendaddress: 'Sent Address',
       walletpassword: 'Wallet Password',
-      accounterrmsg: 'Incorrect account address.',
-      amounterrmsg: 'Amount must less than balance.',
-      paterrmsg: 'Incorrect pat type',
-      gaspricemsg: 'Please input gasPrice',
-      gaspriceerrmsg: 'gas price should greater than {price}',
-      gaslimitmsg: 'Please input gasLimit',
-      gaslimiterrmsg: 'gas limit should greater than {limit}',
-      recipientmsg: 'Please input recipient',
-      amountmsg: 'Please input amount',
-      transactionsubmitted: 'transaction submitted',
-      createtransactionerr: 'create transaction error',
-      tip1: 'Copy The Address',
-      tip2: 'No Transactions yet',
-      tip3: 'Address Copied to ClipBoard.'
+      accounterrmsg: 'Incorrect Account Address',
+      amounterrmsg: 'Amount must be less than or equal to balance',
+      paterrmsg: 'Incorrect pat Type',
+      gaspricemsg: 'Please Enter Gas Price',
+      gaspriceerrmsg: 'Gas price should be greater than or equal to {price}',
+      gaslimitmsg: 'Please Enter Gas Limit',
+      gaslimiterrmsg: 'Gas limit should be greater than or equal to {limit}',
+      recipientmsg: 'Please Input Recipient',
+      amountmsg: 'Please Input Amount',
+      transactionsubmitted: 'Transaction Submitted',
+      createtransactionerr: 'Transaction Error',
+      tip1: 'Copy the Address',
+      tip2: 'No Transactions Yet',
+      tip3: 'Address Copied to Clipboard'
     },
     walletmanage: {
-      walletmanage: 'Wallet Manage',
+      walletmanage: 'Manage Wallet',
       importwallet: 'Import Wallet',
       import: 'Import{name}',
       uploadjson: 'Upload JSON File',
-      deletewallet: 'Delete wallet',
+      deletewallet: 'Delete Wallet',
       changepassword: 'Change password (of wallet)',
-      exportwallet: 'Export wallet as json',
-      setdefaultpayment: 'Set as default payment',
-      placeholder1: 'please input the password of wallet file.'
+      exportwallet: 'Export wallet as JSON',
+      setdefaultpayment: 'Set as Default Payment',
+      placeholder1: 'Please Enter the Wallet Password'
     },
     debits: {
         debits: 'Debits',
@@ -105,9 +105,9 @@ exports.default = {
         freestorage: 'Free storage: {free}/Year',
         bonusamount: 'New user bandwidth： {bonusAmount} GNX',
         updatetime: 'Last update',
-        pricemsg: 'Free 25GB storage is available during the first year, please see billing instructions here',
+        pricemsg: '25GB free storage is available during the first year. Please see billing instructions here.',
         time: 'Time',
-        stroagetraffic: 'Storage/Traffic Used',
+        storagetraffic: 'Storage/Traffic Used',
         wallet: 'Wallet',
         storagefee: 'Storage Fee',
         trafficfee: 'Traffic Fee',
@@ -115,10 +115,10 @@ exports.default = {
     },
     sharestorage: {
       sharestorage: 'Share My Storage',
-      tip1: 'Genaro Sharer is alive, join us and earn reward!',
+      tip1: 'Genaro Sharer is live! Join us and earn rewards!',
       tip2: 'If you want to share your storage space please click\
         <a target="_blank" href="https://www.npmjs.com/package/genaroshare-daemon">this link</a>\
-        for a detailed explanation how to install and run Genaro Sharer.'
+        for a detailed explanation on how to install and run Genaro Sharer.'
     }
   },
   menu: {
